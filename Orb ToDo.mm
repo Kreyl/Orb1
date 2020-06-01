10df90e5349a339c9d495bc70aad1dece6c2935b
kreyl <laystoll@yandex.ru>
2020-06-01 15:57:12 +0300

    + Sent to factory

---
+++ Orb ToDo.mm
@@ -0,0 +1,62 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Orb ToDo" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1591000006449"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Крепление резьбы к базе" POSITION="right" ID="ID_468589704" CREATED="1591000007289" MODIFIED="1591000021257"/>
+<node TEXT="Крепление платы светодиодов" POSITION="right" ID="ID_377875415" CREATED="1591000024948" MODIFIED="1591000035124"/>
+<node TEXT="Крепление 18650" POSITION="right" ID="ID_884721265" CREATED="1591000040308" MODIFIED="1591000046269"/>
+<node TEXT="Крепление батарейного отсека 3хАА" POSITION="right" ID="ID_1033855731" CREATED="1591000046595" MODIFIED="1591000070245"/>
+</node>
+</map>
